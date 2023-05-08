374ddab307f2687db508e194e2a94a23fe784c4f
Davideko <45367400+Davideko212@users.noreply.github.com>
2023-05-08 13:12:54 +0200

    being silly & updates

--- src/main/java/com/github/ancientforgery/ExampleMod.java
+++ src/main/java/com/github/ancientforgery/ExampleMod.java
@@ -2,6 +2,11 @@ package com.github.ancientforgery;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +15,10 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("ancient-forgery");
+	public static final TestBrush TEST_BRUSH =
+			Registry.register(Registries.ITEM, new Identifier("ancient-forgery", "test_brush"),
+					new TestBrush(new FabricItemSettings()
+							.rarity(Rarity.EPIC)));
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/com/github/ancientforgery/TestBrush.java
@@ -0,0 +1,11 @@
+package com.github.ancientforgery;
+
+import net.minecraft.item.BrushItem;
+
+public class TestBrush extends BrushItem {
+    public TestBrush(Settings settings) {
+        super(settings);
+    }
+
+
+}
