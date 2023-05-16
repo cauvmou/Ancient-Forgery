30a73e6fbfddd64fd7d6229bb82f040d20b9581e
cauvmou <cauvmou.8855@gmail.com>
2023-05-16 14:10:31 +0200

    wip

--- src/main/java/com/github/ancient_forgery/data/registry/EntityRegistry.java
+++ src/main/java/com/github/ancient_forgery/data/registry/EntityRegistry.java
@@ -2,18 +2,23 @@ package com.github.ancient_forgery.data.registry;
 
 import com.github.ancient_forgery.data.entity.custom.DecoyPuppetEntity;
 import com.github.ancient_forgery.data.entity.custom.LostSoulEntity;
+import net.fabricmc.fabric.api.biome.v1.BiomeModification;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.fabricmc.fabric.impl.object.builder.FabricEntityType;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.*;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 
 import static com.github.ancient_forgery.main.AncientForgery.MOD_ID;
 
@@ -24,7 +29,7 @@ public class EntityRegistry implements AFRegistry {
             .dimensions(EntityDimensions.fixed(1.0f, 1.75f))
             .build();
     public static final EntityType<LostSoulEntity> LOST_SOUL_ENTITY = FabricEntityTypeBuilder.create(
-            SpawnGroup.CREATURE, LostSoulEntity::new)
+            SpawnGroup.AMBIENT, LostSoulEntity::new)
             .dimensions(EntityDimensions.fixed(0.5f, 0.5f))
             .build();
 
@@ -34,5 +39,19 @@ public class EntityRegistry implements AFRegistry {
         FabricDefaultAttributeRegistry.register(DECOY_PUPPET_ENTITY, DecoyPuppetEntity.setAttributes());
         Registry.register(Registries.ENTITY_TYPE, new Identifier(MOD_ID, "lost_soul"), LOST_SOUL_ENTITY);
         FabricDefaultAttributeRegistry.register(LOST_SOUL_ENTITY, LostSoulEntity.setAttributes());
+        registerEntitySpawn();
+    }
+
+    public void registerEntitySpawn() {
+        BiomeModifications.addSpawn(
+                BiomeSelectors.includeByKey(BiomeKeys.SOUL_SAND_VALLEY),
+                SpawnGroup.AMBIENT,
+                EntityRegistry.LOST_SOUL_ENTITY,
+                25, 2, 5);
+        SpawnRestriction.register(
+                EntityRegistry.LOST_SOUL_ENTITY,
+                SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING,
+                LostSoulEntity::canSpawn);
     }
 }
